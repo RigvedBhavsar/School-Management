67d9da2a207e6b9e1b9df7d5edb613a024673939
RigvedBhavsar <rigvedbhavsar@gmail.com>
2021-08-31 12:07:15 +0530

    Enroolment of student for course modeule Done

---
+++ SchoolManagement/Models/MetaClasses/EnrollmentMetaData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class EnrollmentMetaData
+    {
+        [Display(Name = "Student Grade")]
+        public Nullable<decimal> Grade { get; set; }
+        
+        [Display(Name = "Course")]
+        public int CourseID { get; set; }
+        [Display(Name = "Course")]
+        public Course Course { get; set; }
+        
+        [Display(Name = "Student")]
+        public int StudentID { get; set; }
+        [Display(Name = "Student")]
+        public Student Student { get; set; }
+
+        [Display(Name = "Leacturer")]
+        public Nullable<int> LecturerId { get; set; }
+        [Display(Name = "Leacturer")]
+        public Lecturer Lecturer { get; set; }
+    }
+
+    [MetadataType(typeof(EnrollmentMetaData))]
+    public partial class Enrollment
+    { 
+
+    }
+}
